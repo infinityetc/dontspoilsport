77ee7101fca86b644be3dead707d314ec9e23bbd
root <root@web01.imagenetllc.net>
2013-02-02 19:25:20 -0800

    Put the table stuff back in.

--- include/lib.php
+++ include/lib.php
@@ -25,7 +25,7 @@ function displayGames($games){
   echo '<table>';
 
   foreach($games as $game){
-    
+      
     $score1 = (int)$game->team[0]->scoring->attributes()->points;
     $score2 = (int)$game->team[1]->scoring->attributes()->points;
 
@@ -37,10 +37,11 @@ function displayGames($games){
         echo '<td class="win">';
     }else{
         echo '<td class="lose">';
-    }
+    };
     echo ' '.$score1.'';
     echo '</td></tr>';
 
+    
     echo '<tr class="away"><td class="name">';
     echo $game->team[1]->attributes()->market.' '.$game->team[1]->attributes()->name;
     echo '</td>';
@@ -49,13 +50,54 @@ function displayGames($games){
         echo '<td class="win">';
     }else{
         echo '<td class="lose">';
-    }
+    };
     echo ' '.$score2.'';
-    echo '</td></tr>';
 
-  }
+    echo '</td></tr>';
+          
+  };
 
   echo '</table>';
 
+};
+
+function getNamesToIgnore($hideFilter){
+    $ignore = array();
+    $games = getGames(null, 17, 'REG');
+    foreach($games as $game){
+        $team1 = $game->team->attributes()->id;
+        $team2 = $game->team[1]->attributes()->id;
+        
+        
+        if($team1 == $hideFilter || is_array($hideFilter) && array_search($team1, $hideFilter) !== FALSE){
+            $ignore[] = (string)$game->team[0]->attributes()->market;
+            $ignore[] = (string)$game->team[0]->attributes()->name;
+        }
+        
+        if($team2 == $hideFilter || is_array($hideFilter) && array_search($team2, $hideFilter) !== FALSE){
+            $ignore[] = (string)$game->team[1]->attributes()->market;
+            $ignore[] = (string)$game->team[1]->attributes()->name;
+        }
+    }
+    return $ignore;
+}
+
+function displayRecaps($filterIgnore){
+    $xml = simplexml_load_file('http://api.sportsdatallc.org/content-nfl-t1/recaps?api_key=y8vaxqmuatj64dyjt2qfzppx','SimpleXMLElement',LIBXML_NOCDATA|LIBXML_DTDLOAD |LIBXML_PARSEHUGE );
+    foreach($xml->channel->item as $item){
+        //echo $item->category;die;
+
+        foreach($item->category as $cat){
+            foreach($filterIgnore as $filter){
+                if(stripos($cat,$filter) !== FALSE){
+                    continue 3;die;
+                }
+            }
+        }
+        echo '<h3><a href="javascript:void(0)" onclick="$(this).parent().next().toggle(\'slow\');$(this).parent().next().next().toggle(\'slow\');">'.$item->title.'</a></h3>';
+        echo '<div>'.str_replace('[...]','[ <a href="javascript:void(0)" onclick="$(this).parent().toggle(\'slow\');$(this).parent().next().toggle(\'slow\');">read more</a> ]', strip_tags($item->description)).'</div>';
+        echo '<div style="display:none;">'.$item->children('content', true).'</div>';
+    }
+    
 }
 ?>
