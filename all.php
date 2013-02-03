794956569075bf4568c1f38284a14336667a2a40
root <root@web01.imagenetllc.net>
2013-02-02 19:13:40 -0800

    Just in case all, index, and style.

--- all.php
+++ all.php
@@ -1,6 +1,7 @@
 <?php
 include_once('include/header.php');
 include('include/lib.php');
+include('include/lib2.php');
 ?>
 
 <div class="scores">
@@ -12,53 +13,75 @@ include('include/lib.php');
 <?php
 $self = preg_replace('#&week=[^&]+#','',$_SERVER['REQUEST_URI']);
 $self = preg_replace('#&season=[^&]+#','',$self);
+if(stripos($self,'?') === false){
+   $self .= '?';
+}
 ?>
-
-<div id="week">Regular Season - Week: 
-<a href="<?php echo $self ?>&season=REG&week=17">17</a>
-<a href="<?php echo $self ?>&season=REG&week=16">16</a>
-<a href="<?php echo $self ?>&season=REG&week=15">15</a>
-<a href="<?php echo $self ?>&season=REG&week=14">14</a>
-<a href="<?php echo $self ?>&season=REG&week=13">13</a>
-<a href="<?php echo $self ?>&season=REG&week=12">12</a>
-<a href="<?php echo $self ?>&season=REG&week=11">11</a>
-<a href="<?php echo $self ?>&season=REG&week=10">10</a>
-<a href="<?php echo $self ?>&season=REG&week=9">9</a>
-<a href="<?php echo $self ?>&season=REG&week=8">8</a>
-<a href="<?php echo $self ?>&season=REG&week=7">7</a>
-<a href="<?php echo $self ?>&season=REG&week=6">6</a>
-<a href="<?php echo $self ?>&season=REG&week=5">5</a>
-<a href="<?php echo $self ?>&season=REG&week=4">4</a>
-<a href="<?php echo $self ?>&season=REG&week=3">3</a>
-<a href="<?php echo $self ?>&season=REG&week=2">2</a>
-<a href="<?php echo $self ?>&season=REG&week=1">1</a>
-<br />
-Post Season - Week: 
-<a href="<?php echo $self ?>&season=PST&&week=3">3</a>
-<a href="<?php echo $self ?>&season=PST&&week=2">2</a>
-<a href="<?php echo $self ?>&season=PST&&week=1">1</a>
-</div>
 
 <?php
 $week=@$_GET['week'];
 if(empty($week)){$week=3;}
 $season=@$_GET['season'];
 if(empty($_GET['season'])){$season='PST';}
+
 ?>
 
-<h3>Week <?php echo $week; ?></h3>
-<div id="scores">
+<a id="change" href="javascript:void(0);">Change Week</a>
+
+<div id="week">
+Regular Season - Week: <br>
+<?php
+$i = 17;
+while ($i > 0) {
+	echo '<a href="' . $self . '&season=REG&week=' . $i .'"';
+	if ($i == $week && $season == 'REG') {
+		echo ' class="selected">' . $i . '</a>';
+	} else {
+		echo '>' . $i . '</a>';
+	};
+	$i--;
+};
+?>
+
+<br>
+Post Season - Week:<br> 
+<?php
+$i = 3;
+while ($i > 0) {
+	echo '<a href="' . $self . '&season=PST&week=' . $i .'"';
+	if ($i == $week && $season == 'PST') {
+		echo ' class="selected">' . $i . '</a>';
+	} else {
+		echo '>' . $i . '</a>';
+	};
+	$i--;
+};
+?>
+</div>
+
+<?php
+if ($season == 'REG') {
+	echo '<h2>Regular Season - Week ' . $week . '</h2>';
+} else {
+	echo '<h2>Post Season - Week ' . $week . '</h2>';
+};
+?>	
 
 <?php
 displayGames(getGames(@$_GET['nfl'], $week, $season));
 ?>
 
+</div><!-- .scores -->
+
 </div><!-- .unspoil -->
 
-</div><!-- .scores -->
 
 
 <div class="news">
+
+<?php
+displayNews(@$_GET['nfl']);
+?>
 
 </div><!-- .news -->
 
--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 require_once('include/header.php');
 ?>
 
-<p>Don't spoil their ending for me:</p>
+<p>Select the teams you don't want to hear about</p>
+
+<a class="jump" href="#AFC-N">AFC-North</a>
+<a class="jump" href="#AFC-S">AFC-South</a>
+<a class="jump" href="#AFC-E">AFC-East</a>
+<a class="jump" href="#AFC-W">AFC-West</a>
+<a class="jump" href="#NFC-N">NFC-North</a>
+<a class="jump" href="#NFC-S">NFC-South</a>
+<a class="jump" href="#NFC-E">NFC-East</a>
+<a class="jump" href="#NFC-W">NFC-West</a>
 
 <form action="all.php" method="get">
 
-<h2>AFC-North</h2>
+<h2 id="AFC-N">AFC-North</h2>
 	<input type="checkbox" name="nfl[]" id="BAL" value="BAL">
 	<label for="BAL">Baltimore Ravens</label>
 	<input type="checkbox" name="nfl[]" id="CIN" value="CIN">
@@ -16,7 +25,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="PIT" value="PIT">
 	<label for="PIT">Pittsburgh Steelers</label>
 
-<h2>AFC-South</h2>
+<h2 id="AFC-S">AFC-South</h2>
 	<input type="checkbox" name="nfl[]" id="HOU" value="HOU">
 	<label for="HOU">Houston Texans</label>
 	<input type="checkbox" name="nfl[]" id="IND" value="IND">
@@ -26,7 +35,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="TEN" value="TEN">
 	<label for="TEN">Tennesse Titans</label>
 
-<h2>AFC-East</h2>
+<h2 id="AFC-E">AFC-East</h2>
 	<input type="checkbox" name="nfl[]" id="BUF" value="BUF">
 	<label for="BUF">Buffalo Bills</label>
 	<input type="checkbox" name="nfl[]" id="MIA" value="MIA">
@@ -36,7 +45,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="NYJ" value="NYJ">
 	<label for="NYJ">New York Jets</label>
 
-<h2>AFC-West</h2>
+<h2 id="AFC-W">AFC-West</h2>
 	<input type="checkbox" name="nfl[]" id="DEN" value="DEN">
 	<label for="DEN">Denver Broncos</label>
 	<input type="checkbox" name="nfl[]" id="KC" value="KC">
@@ -46,7 +55,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="SD" value="SD">
 	<label for="SD">San Diego Chargers</label>
 
-<h2>NFC-North</h2>
+<h2 id="NFC-N">NFC-North</h2>
 	<input type="checkbox" name="nfl[]" id="CHI" value="CHI">
 	<label for="CHI">Chicago Bears</label>
 	<input type="checkbox" name="nfl[]" id="DET" value="DET">
@@ -56,7 +65,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="MIN" value="MIN">
 	<label for="MIN">Minnesota Vikings</label>
 
-<h2>NFC-South</h2>
+<h2 id="NFC-S">NFC-South</h2>
 	<input type="checkbox" name="nfl[]" id="ATL" value="ATL">
 	<label for="ATL">Atlanta Falcons</label>
 	<input type="checkbox" name="nfl[]" id="CAR" value="CAR">
@@ -66,7 +75,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="TB" value="TB">
 	<label for="TB">Tampa Bay Buccaneers</label>
 
-<h2>NFC-East</h2>
+<h2 id="NFC-E">NFC-East</h2>
 	<input type="checkbox" name="nfl[]" id="DAL" value="DAL">
 	<label for="DAL">Dallas Cowboys</label>
 	<input type="checkbox" name="nfl[]" id="NYG" value="NYG">
@@ -76,7 +85,7 @@ require_once('include/header.php');
 	<input type="checkbox" name="nfl[]" id="WAS" value="WAS">
 	<label for="WAS">Washington Redskins</label>
 
-<h2>NFC-West</h2>
+<h2 id="NFC-W">NFC-West</h2>
 	<input type="checkbox" name="nfl[]" id="ARI" value="ARI">
 	<label for="ARI">Arizona Cardinals</label>
 	<input type="checkbox" name="nfl[]" id="SF" value="SF">
